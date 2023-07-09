a0248613701d584a08ed5147836d5d27da386215
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-07-10 00:15:00 +0530

    Changes to Api  Url

--- clients/api.ts
+++ clients/api.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
-  "https://d2vcaeil8kefe2.cloudfront.net/graphql",
+  process.env.NEXT_PUBLIC_API_URL as string,
   {
     headers: () => ({
       Authorization: isClient
